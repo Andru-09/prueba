3731670aa773717a7fc44f059923c137a6a79152
Andru-09 <andrecruz@cecyteqroo.edu.mx>
2024-05-30 14:01:16 -0500

    Add files via upload

---
+++ apiryapp3.0/cvauto.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+CHCP 65001 > NUL
+
+call C:\Users\andre\AppData\Local\Programs\Python\Python312\activate.bat
+cd "C:\Users\andre\OneDrive\Escritorio\apiryapp3.0\__pycache__"
+python cv.py
+
+exit
+
+PAUSE > nul
